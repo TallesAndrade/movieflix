660a71b97b00595b0765f087422daa2a04709469
Talles Andrade <tallesandrade7@gmail.com>
2025-08-20 15:46:44 -0300

    build(config): Carrega variáveis de ambiente do .env na inicialização

--- src/main/java/com/movieflix/movieflix/MovieflixApplication.java
+++ src/main/java/com/movieflix/movieflix/MovieflixApplication.java
@@ -1,5 +1,6 @@
 package com.movieflix.movieflix;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieflixApplication {
 
     public static void main(String[] args) {
+        Dotenv env = Dotenv.load();
+        env.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(MovieflixApplication.class, args);
     }
 
